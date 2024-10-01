533a98e84da089fcddb96bb6b4f04eef81332075
Tamanegi-K <43199511+Tamanegi-K@users.noreply.github.com>
2024-10-01 16:56:13 +0800

    Camera zoom part 1
    Can still go through terrain - it should not do that

--- GoatClimbGame/Assets/Scripts/PlayerController.cs
+++ GoatClimbGame/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,20 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     [Header("Component Inputs")]
-    public GoatControls goatControls;
-    public Transform camPivot, orientation, model;
-    public Rigidbody rb;
+    private GoatControls goatControls;
+    private Transform camPivot, orientation, model;
+    private Rigidbody rb;
 
     [Header("Variables")]
     public int herbCount = 0;
-    public Vector2 sensitivity = new Vector2(15f, 12f); // x is Left/Right, y is Up/Down. THIS DOES NOT CORRELATE TO CAM ROTATION THOUGH
-    public float lookLimitUp = -40f, lookLimitDown = 10f;
-    public float rotX, rotY, rotLerpX, rotLerpY;
+    public Vector3 sensitivity = new Vector3(20f, 15f, 8f); // x is Left/Right, y is Up/Down, z is Scroll wheel THIS DOES NOT CORRELATE TO CAM ROTATION THOUGH
+    public Vector2 lookLimit = new Vector2(-40f, 10f); // x = upwards, y = downwards
+    public Vector2 zoomLimit = new Vector2(0.12f, 1.5f); // x = lowest, y = highest
+    public Vector2 camYPos = new Vector2(0.55f, 4.5f); // x = lowest, y = highest
+    private float rotX, rotY, rotLerpX, rotLerpY, zoomVal = 0.5f, zoomLerp = 0.5f;
     public Vector3 moveDir, modelRotLerp;
     private Vector2 kbInputs;
-    public float speedDefault = 1f, speedMax = 4f;
+    public float speedDefault = 5f, speedMax = 4f;
 
     void OnEnable()
     {
@@ -42,6 +44,8 @@ public class PlayerController : MonoBehaviour
 
         orientation = GameObject.Find("Orientation").transform;
         model = GameObject.Find("PlayerModel").transform;
+
+        camPivot = GameObject.Find("CameraPivot").transform;
     }
 
     // Update is called once per frame
@@ -53,7 +57,11 @@ public class PlayerController : MonoBehaviour
 
         rotY += mouseLR;
         rotX -= mouseUD;
-        rotX = Mathf.Clamp(rotX, lookLimitUp, lookLimitDown);
+        rotX = Mathf.Clamp(rotX, lookLimit.x, lookLimit.y);
+
+        float mouseW = goatControls.Defaults.CamZoomer.ReadValue<float>();
+        zoomVal = Mathf.Clamp(zoomVal + (mouseW * Time.deltaTime * sensitivity.z), zoomLimit.x, zoomLimit.y);
+
         MoveCamBasedOnInputs();
 
         // Keyboard Inputs to Code
@@ -73,7 +81,7 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(moveDir.normalized * speedDefault * 10f, ForceMode.Force);
 
         // Rotate model based on movement direction
-        modelRotLerp = Vector3.Lerp(modelRotLerp, moveDir.normalized, Time.deltaTime * 3f);
+        modelRotLerp = Vector3.Lerp(modelRotLerp, moveDir.normalized, Time.deltaTime * 2.7f);
         if (moveDir != Vector3.zero)
         {
             model.transform.forward = modelRotLerp;
@@ -86,17 +94,31 @@ public class PlayerController : MonoBehaviour
         if (Mathf.Abs(rotLerpX - rotX) <= 0.08f)
             rotLerpX = rotX;
         else
-            rotLerpX = Mathf.Lerp(rotLerpX, rotX, Time.deltaTime * 5f);
+            rotLerpX = Mathf.Lerp(rotLerpX, rotX, Time.deltaTime * 6.9f);
 
         if (Mathf.Abs(rotLerpY - rotY) <= 0.08f)
             rotLerpY = rotY;
         else
-            rotLerpY = Mathf.Lerp(rotLerpY, rotY, Time.deltaTime * 5f);
+            rotLerpY = Mathf.Lerp(rotLerpY, rotY, Time.deltaTime * 6.9f);
 
         // The actual camera rotating parts
         camPivot.transform.localRotation = Quaternion.Euler(rotLerpX, rotLerpY, 0f);
 
         // Set orientation based on camera's current position
         orientation.rotation = Quaternion.Euler(0, camPivot.rotation.eulerAngles.y, 0);
+
+        // Zoom based on value
+        if (Mathf.Abs(zoomLerp - zoomVal) < 0.002f)
+            zoomLerp = zoomVal;
+        else
+            zoomLerp = Mathf.Lerp(zoomLerp, zoomVal, Time.deltaTime * 6.9f);
+
+        camPivot.localScale = new Vector3(zoomLerp, zoomLerp, zoomLerp);
+
+        // Camera position based on zoom
+        // y = mx + b, x is zoomLimit, y is camYPos
+        float m = (camYPos.y - camYPos.x) / (zoomLimit.y - zoomLimit.x); // slope
+        float b = camYPos.y - (m * zoomLimit.y);
+        camPivot.localPosition = new Vector3(0f, (m * zoomLerp) + b, 0f);
     }
 }
